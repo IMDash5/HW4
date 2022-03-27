74d8b16a9856a4dd58b55228ec57fd029ea12f3a
Ivan <mmavrin80@gmail.com>
2022-03-27 20:34:18 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿int Parse(string message)
+{
+    Console.WriteLine(message);
+    string strNum = Console.ReadLine();
+    int Num = int.Parse(strNum);
+    return Num;
+}
+
+double Num1 = Parse("Введите первое число: ");
+double Num2 = Parse("Введите второе число: ");
+double result = 0;
+
+if (Num2 >= 0)
+{
+    result = Math.Pow(Num1, Num2);
+    Console.WriteLine(result);
+}
+else 
+{
+    result = Math.Pow(Num1, -Num2);
+    Console.WriteLine(result);
+}
