34c1508e7af639f66a0e93f8470db91d2987088d
Ivan <mmavrin80@gmail.com>
2022-03-27 21:17:46 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿int Parse(string message)
+{
+    Console.WriteLine(message);
+    string strNum = Console.ReadLine();
+    int Num = int.Parse(strNum);
+    return Num;
+}
+int Number = Parse("Введите число: ");
+string strNumber = ($"{Number}");
+int length = strNumber.Length;
+int result = 0;
+
+if (Number >= 0)
+{
+    for (int i = 0; i < length; i++)
+    {
+        result = result + int.Parse(Char.ToString (strNumber[i]));
+    }
+}
+else
+{
+    for (int i = 1; i < length ; i++)
+    {
+        result = result + int.Parse(Char.ToString (strNumber[i]));
+    }
+}
+Console.WriteLine(result);
